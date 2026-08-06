2b3fdb024a53c2e43ce01804564ab780aa81f9c0
unknown <hassan.monirul@gmail.com>
2026-08-06 15:41:36 +0600

    app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,8 @@
-import express, { NextFunction, Request, response, Response } from "express";
+import app from "./app";
 import config from "./config";
-import initDB, { pool } from "./config/db";
-import logger from "./middleware/logger";
-import { userRoutes } from "./modules/user/user.routes";
-import { todoRoutes } from "./modules/todo/todo.routes";
-import { authRoutes } from "./modules/auth/auth.routes";
 
-const app = express();
 const port = config.port;
 
-// parser
-app.use(express.json());
-
-initDB();
-
-app.get("/", logger, (req: Request, res: Response) => {
-  res.send("Hello Next Level Developers!");
-});
-
-app.use("/users", userRoutes);
-
-app.use("/todos", todoRoutes);
-
-app.use("/auth", authRoutes);
-
-//? not found route
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-    path: req.path,
-  });
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
